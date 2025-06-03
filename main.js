c863a4f7bc602828fe2a7679b9a88ee4d2546e4a
moloudii <mhmmdalinaz@gmail.com>
2025-06-03 10:47:15 +0330

    Compatibility and update to version 36

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const Parser = require("rss-parser");
 require("electron-reload")(__dirname);
 
 let win;
@@ -10,11 +11,21 @@ const createMainWindow = () => {
     alwaysOnTop: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
-      sandbox: false,
+      sandbox: true, // Electron 36
     },
   });
   win.loadFile("./index.html");
 };
+// 📡 IPC Listener: get Feed request from renderer { electron 36 }
+ipcMain.handle("rss:load", async (event, url) => {
+  try {
+    const parser = new Parser();
+    const feed = await parser.parseURL(url);
+    return feed;
+  } catch (error) {
+    return { error: error.message };
+  }
+});
 app.whenReady().then(() => {
   createMainWindow();
 });
--- renderer.js
+++ renderer.js
@@ -1,2 +1,6 @@
 const rssUrl = "https://www.asriran.com/fa/rss/allnews";
-window.loadRssFeed(rssUrl).then((response) => console.log(response));
+// window.loadRssFeed(rssUrl).then((response) => console.log(response));
+// Electron 36
+window.rssApi.loadRssFeed(rssUrl).then((data) => {
+  console.log(data);
+});
